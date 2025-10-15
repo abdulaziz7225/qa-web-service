2a79dda215110e9203d9c3bb5a47618502a62125
Abdulaziz Musaev <amusaev7225@gmail.com>
2025-10-15 04:18:00 +0200

    Handle CORS headers

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use serde::Serialize;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
 
-use warp::{Filter, reject::Reject, Rejection, Reply, http::StatusCode};
+use warp::{
+    Filter, Rejection, Reply, filters::cors::CorsForbidden, http::Method, http::StatusCode,
+    reject::Reject,
+};
 
 #[derive(Debug, Serialize)]
 struct Question {
@@ -56,30 +59,38 @@ async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
 }
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
-    if let Some(InvalidId) = r.find() {
-        Ok(warp::reply::with_status("No valid ID presented", StatusCode::UNPROCESSABLE_ENTITY,))
+    if let Some(error) = r.find::<CorsForbidden>() {
+        Ok(warp::reply::with_status(
+            error.to_string(),
+            StatusCode::FORBIDDEN,
+        ))
+    } else if let Some(InvalidId) = r.find() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented".to_string(),
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
     } else {
-        Ok(warp::reply::with_status("Route not found", StatusCode::NOT_FOUND))
+        Ok(warp::reply::with_status(
+            "Route not found".to_string(),
+            StatusCode::NOT_FOUND,
+        ))
     }
 }
 
-
 #[tokio::main]
 async fn main() {
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("content-type")
+        .allow_methods(&[Method::GET, Method::POST, Method::PUT, Method::DELETE]);
+
     let get_items = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
         .and_then(get_questions)
         .recover(return_error);
 
-    let routes = get_items;
+    let routes = get_items.with(cors);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
-
-    // let hello = warp::get()
-    //     .map(|| format!("Hello, World!"));
-
-    // warp::serve(hello)
-    //     .run(([127, 0, 0, 1], 3030))
-    //     .await;
 }
