509d9df8f65428f7517f896e37a0d3cbd4ebb874
Abdulaziz Musaev <amusaev7225@gmail.com>
2025-10-15 02:22:55 +0200

    Implement Display and Debug traits manually

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+struct QuestionId(String);
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No ID provided")),
+        }
+    }
+}
+
+impl std::fmt::Display for QuestionId {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(f, "id: {}", self.0)
+    }
+}
+
+impl std::fmt::Debug for Question {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "{}, title: {}, content: {}, tags: {:?}",
+            self.id, self.title, self.content, self.tags
+        )
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No ID provided"),
+        String::from("First question"),
+        String::from("Content of question"),
+        Some(vec![String::from("faq")]),
+    );
+    println!("{question:?}");
 }
