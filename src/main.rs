b8bb63a9a581f75261c1d42ebd20508833c949ca
Abdulaziz Musaev <amusaev7225@gmail.com>
2025-10-16 05:24:22 +0200

    Add deleting a question API

--- src/main.rs
+++ src/main.rs
@@ -148,6 +148,13 @@ async fn update_question(
     Ok(warp::reply::with_status("Question updated", StatusCode::OK))
 }
 
+async fn delete_question(id: String, store: Store) -> Result<impl warp::Reply, warp::Rejection> {
+    match store.questions.write().await.remove(&QuestionId(id)) {
+        Some(_) => return Ok(warp::reply::with_status("Question deleted", StatusCode::OK)),
+        None => return Err(warp::reject::custom(Error::QuestionNotFound)),
+    }
+}
+
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
     if let Some(error) = r.find::<Error>() {
         let code = match error {
@@ -206,9 +213,17 @@ async fn main() {
         .and(warp::body::json())
         .and_then(update_question);
 
+    let delete_question = warp::delete()
+        .and(warp::path("questions"))
+        .and(warp::path::param::<String>())
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and_then(delete_question);
+
     let routes = get_questions
         .or(add_question)
         .or(update_question)
+        .or(delete_question)
         .with(cors)
         .recover(return_error);
 
